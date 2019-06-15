3c41c9e2e211b0ba576c237d0dc02542ba1133ae
Paul DeYonghe <p.deyonghe@gmail.com>
2019-06-15 00:30:09 -0500

    got get post/:id/comments working

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -31,4 +31,20 @@ router.get('/:id', async (req, res) => {
     }
 })
 
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const post = await Posts.findById(req.params.id);
+        const comments = await Posts.findPostComments(req.params.id);
+        console.log(req.params.id);
+        if (post) {
+            res.status(200).json(comments);
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
+    }
+    catch {
+        res.status(500).json({ error: "The comments information could not be retrieved." });
+    }
+})
+
 module.exports = router;
